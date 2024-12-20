e44064e64900b199c6faefc19e147a0e6132b7ec
NureDukhotaIvan <ivan.dukhota@nure.ua>
2024-12-20 19:26:53 +0200

    Update admin.py

--- Task2/arkpz-pzpi-22-6-dukhota-ivan-task2/admin.py
+++ Task2/arkpz-pzpi-22-6-dukhota-ivan-task2/admin.py
@@ -1,1 +1,35 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import (
+    User, Student, Parent, Staff, Sensor,
+    Camera, Incident, Attendance, Notification
+)
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Roles', {'fields': ('role',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Contact info', {'fields': ('email',)}),  
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'role', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'role', 'is_staff')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Student)
+admin.site.register(Parent)
+admin.site.register(Staff)
+admin.site.register(Sensor)
+admin.site.register(Camera)
+admin.site.register(Incident)
+admin.site.register(Attendance)
+admin.site.register(Notification)
 
