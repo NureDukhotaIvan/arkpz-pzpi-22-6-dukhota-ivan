f0e500b8689d96b63d5cd23e1ba2d3c69a9145d4
NureDukhotaIvan <ivan.dukhota@nure.ua>
2024-12-20 19:25:20 +0200

    Update serializers.py

--- Task2/arkpz-pzpi-22-6-dukhota-ivan-task2/serializers.py
+++ Task2/arkpz-pzpi-22-6-dukhota-ivan-task2/serializers.py
@@ -1,1 +1,24 @@
 
+# api/permissions.py
+
+from rest_framework import permissions
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+class IsStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'staff'
+
+class IsParent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'parent'
+
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'student'
+
+class IsAdminOrStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and (request.user.is_staff or request.user.role == 'staff')
